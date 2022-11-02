afe98c9817bb2ba6469abcc8aeb5ccf93f7e1db2
oshanQQ <oshanqq@gmail.com>
2022-11-02 10:56:34 +0900

    fix: Fixed deploy-with-helm.yml because values-full.yml file could not be loaded.
    The "\" was not written. Therefore, each command was not connected by "\".

--- deploy-with-helm.sh
+++ deploy-with-helm.sh
@@ -1,6 +1,6 @@
 helm  upgrade --install  enterprise-gateway \
   etc/kubernetes/helm/enterprise-gateway \
   --kube-context kubernetes-admin@kubernetes \
-  --namespace enterprise-gateway
+  --namespace enterprise-gateway \
   -f values-full.yml
 
